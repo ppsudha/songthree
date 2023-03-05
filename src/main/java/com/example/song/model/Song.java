4232e7466ab395f5618c3d3b5df4b3af51b8ea07
ppsudha <pasupuletisudhanaidu@gmail.com>
2023-03-05 11:03:42 +0000

    solution

---
+++ src/main/java/com/example/song/model/Song.java
@@ -0,0 +1,69 @@
+/*
+ * 
+ * You can use the following import statements
+ * 
+ * import javax.persistence.*;
+ * 
+ */
+
+// Write your code here
+package com.example.song.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "song")
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "songid")
+    private int songId;
+    @Column(name = "songName")
+    private String songName;
+     @Column(name = "lyricist")
+    private String lyricist;
+     @Column(name = "singer")
+    private String singer;
+     @Column(name = "musicDirector")
+    private String musicDirector;
+    public Song() {
+  
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getLyricist() {
+        return lyricist;
+    }
+
+    public void setLyricist(String lyricist) {
+        this.lyricist = lyricist;
+    }
+public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+    public String getMusicDirector() {
+        return musicDirector;
+    }
+
+    public void setMusicDirector(String musicDirector) {
+        this.musicDirector = musicDirector;
+    }
+}
